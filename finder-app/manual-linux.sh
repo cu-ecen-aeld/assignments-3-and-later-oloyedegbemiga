292d96faadd951fc7bee992514065581215531e2
oloyedegbemiga <gbemigao@seas.upenn.edu>
2026-07-08 12:17:32 -0400

    full test success

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -36,8 +36,11 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
 
     # TODO: Add your kernel build steps here
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make -j"$(nproc)" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} Image
+
+    cp arch/${ARCH}/boot/Image "${OUTDIR}/Image"
 fi
+# cp arch/${ARCH}/boot/Image "${OUTDIR}/Image"
 
 echo "Adding the Image in outdir"
 
@@ -50,9 +53,11 @@ then
 fi
 
 # TODO: Create necessary base directories
-mkdir -p "${OUTDIR}/bin" "${OUTDIR}/dev" "${OUTDIR}/etc" "${OUTDIR}/home" "${OUTDIR}/lib" "${OUTDIR}/lib64" "${OUTDIR}/proc" "${OUTDIR}/sbin" "${OUTDIR}/sys" "${OUTDIR}/tmp" "${OUTDIR}/usr" "${OUTDIR}/var"
-mkdir -p "${OUTDIR}/usr/bin" "${OUTDIR}/usr/bin" "${OUTDIR}/usr/sbin"
-mkdir -p "${OUTDIR}/var/log"
+mkdir -p "${OUTDIR}/rootfs"
+
+mkdir -p "${OUTDIR}/rootfs"/{bin,dev,etc,home,lib,lib64,proc,sbin,sys,tmp,usr,var}
+mkdir -p "${OUTDIR}/rootfs"/usr/{bin,sbin}
+mkdir -p "${OUTDIR}/rootfs/var/log"
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -69,15 +74,29 @@ else
 fi
 
 # TODO: Make and install busybox
-make -j"${nproc}" ARCH=${ARCH} CROSS_COMPLILE=${CROSS_COMPILE}
-make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPLILE=${CROSS_COMPILE} install 
+
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} distclean
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+
+sed -i 's/^CONFIG_TC=y/# CONFIG_TC is not set/' .config
+
+yes "" | make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} oldconfig
+make -j"$(nproc)" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+
+make -j"$(nproc)" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+
+${CROSS_COMPILE}readelf -a busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a busybox | grep "Shared library"
+
+INTERPRETER="$(${CROSS_COMPILE}readelf -a busybox | sed -n 's/.*Requesting program from interpreter: \(.*\)/\1/p')"
 
 # TODO: Add library dependencies to rootfs
-INTERPRETER="$(${CROSS_COMPILE}readelf -a bin/busybox | sed -n 's/.*Requesting program from interpreter: \(.*\)/\1/p')"
+# INTERPRETER="$(${CROSS_COMPILE}readelf -a bin/busybox | sed -n 's/.*Requesting program from interpreter: \(.*\)/\1/p')"
+INTERPRETER="$(${CROSS_COMPILE}readelf -a busybox | sed -n 's/.*Requesting program interpreter: \(.*\)]/\1/p')"
 SYSROOT="$(${CROSS_COMPILE}gcc -print-sysroot)"
 
 if [ -n "${INTERPRETER}" ]
@@ -86,21 +105,58 @@ if [ -n "${INTERPRETER}" ]
         cp -L "${SYSROOT}${INTERPRETER}" "${OUTDIR}/rootfs${INTERPRETER}"
     fi
 
-for LIBRARY in $(${CROSS_COMPILE}readelf -d bin/busybox | sed -n 's/.*Shared library: \[\(.*\)\]/\1/p')
+for BIN in "${OUTDIR}/busybox/busybox" "${OUTDIR}/rootfs/home/writer"
 do
-    LIBRARY_PATH="$(find "${SYSROOT}/lib" "${SYSROOT}/usr/lib" -name ${LIBRARY} -print -quit)"
-    mkdir -p "${OUTDIR}/rootfs$(dirname ${LIBRARY#"${SYSROOT}"})"
-    cp -L "${LIBRARY_PATH}" "${OUTDIR}/rootfs${LIBRARY_PATH#"${SYSROOT}"}"
+    INTERPRETER="$(${CROSS_COMPILE}readelf -a "$BIN" | sed -n 's/.*Requesting program interpreter: \(.*\)]/\1/p')"
 
+    if [ -n "${INTERPRETER}" ]; then
+        mkdir -p "${OUTDIR}/rootfs$(dirname "${INTERPRETER}")"
+        cp -L "${SYSROOT}${INTERPRETER}" "${OUTDIR}/rootfs${INTERPRETER}"
+    fi
+
+    for LIBRARY in $(${CROSS_COMPILE}readelf -d "$BIN" | sed -n 's/.*Shared library: \[\(.*\)\]/\1/p')
+    do
+        LIBRARY_PATH="$(find "${SYSROOT}" -name "${LIBRARY}" -print -quit)"
+        mkdir -p "${OUTDIR}/rootfs$(dirname "${LIBRARY_PATH#"${SYSROOT}"}")"
+        cp -L "${LIBRARY_PATH}" "${OUTDIR}/rootfs${LIBRARY_PATH#"${SYSROOT}"}"
+    done
 done
 
+
 # TODO: Make device nodes
+sudo mknod -m 666 "${OUTDIR}/rootfs/dev/null" c 1 3
+sudo mknod -m 666 "${OUTDIR}/rootfs/dev/console" c 5 1
 
 # TODO: Clean and build the writer utility
+make -C "${FINDER_APP_DIR}" clean
+make -C "${FINDER_APP_DIR}" CROSS_COMPILE="${CROSS_COMPILE}"
+
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
 
+cp "${FINDER_APP_DIR}/finder.sh" "${OUTDIR}/rootfs/home/"
+cp "${FINDER_APP_DIR}/writer" "${OUTDIR}/rootfs/home/"
+cp "${FINDER_APP_DIR}/finder-test.sh" "${OUTDIR}/rootfs/home/"
+cp "${FINDER_APP_DIR}/autorun-qemu.sh" "${OUTDIR}/rootfs/home/"
+
+
+mkdir -p "${OUTDIR}/rootfs/home"
+cp -rL "${FINDER_APP_DIR}/conf" "${OUTDIR}/rootfs/home/"
+cp -rL "${FINDER_APP_DIR}/conf" "${OUTDIR}/rootfs/"
+
+
+chmod +x "${OUTDIR}/rootfs/home/finder.sh"
+chmod +x "${OUTDIR}/rootfs/home/finder-test.sh"
+chmod +x "${OUTDIR}/rootfs/home/writer"
+chmod +x "${OUTDIR}/rootfs/home/autorun-qemu.sh"
+
+
+
 # TODO: Chown the root directory
+sudo chown -R root:root "${OUTDIR}/rootfs"
 
 # TODO: Create initramfs.cpio.gz
+cd "${OUTDIR}/rootfs"
+find . | cpio -H newc -ov --owner root:root > "${OUTDIR}/initramfs.cpio"
+gzip -f "${OUTDIR}/initramfs.cpio"
